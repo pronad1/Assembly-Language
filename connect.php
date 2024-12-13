d8524ef064c4a69ccdce388b1c40770a1db7a4d2
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-12-13 16:51:59 +0600

    12/13/24

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect('localhost', 'root', '7034', 'registrationdata');
+$con = mysqli_connect('localhost', 'root', '', 'registrationdata');
 
 if (!$con) {
     die("Connection Failed:" . mysqli_connect_error());
