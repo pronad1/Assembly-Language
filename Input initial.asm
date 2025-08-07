a49b62589266738d2c39e9e9fb4968a0ae1d133a
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-08-07 19:16:24 +0600

    ok

--- Input initial.asm
+++ Input initial.asm
@@ -1,13 +1,63 @@
+.model small 
+.stack 100h
+.data
+a db 'Enter three initials: $'
+.code
 
-; You may customize this and other start-up templates; 
-; The location of this template is c:\emu8086\inc\0_com_template.txt
-
-org 100h
-
-; add your code here
-
-ret
-
-
-
-
+main proc
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,9
+    lea dx,a
+    int 21h
+    
+    mov ah,1
+    int 21h 
+    mov bl,al 
+    
+    mov ah,1
+    int 21h
+    mov bh,al
+    
+    mov ah,1
+    int 21h
+    mov cl,al
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl, 13
+    int 21h
+    
+    mov ah,2
+    mov dl,bl
+    int 21h
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl, 13
+    int 21h
+    
+    mov ah,2
+    mov dl,bh
+    int 21h
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl, 13
+    int 21h
+    
+    mov ah,2
+    mov dl,cl
+    int 21h
+   
+    
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
